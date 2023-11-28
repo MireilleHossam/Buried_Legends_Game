c5c747ef1660b2b1e5ea2fc25d510fcd9d4dd71c
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-11-28 23:34:52 +0200

    Adding torch scripts to light it up

---
+++ Assets/Scripts/Flammable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Flammable : MonoBehaviour
+{
+    private bool isOnFire = false;
+    [SerializeField]
+    private GameObject flameVisuals;
+    
+
+
+    private void OnEnable()
+    {
+        
+    }
+    public void SetOnFire()
+    {
+        flameVisuals.SetActive(true);
+        isOnFire = true;
+        EventsManager.TrackLitTorches();
+    }
+
+    public bool IsOnFire()
+    {
+        return isOnFire;
+    }
+}
