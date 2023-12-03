4aaccfbb8db7a23fedd1e12255ab9982208cb396
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-12-03 12:09:09 +0200

    Adding grab interactable of sword

--- Assets/MothBoids.cs
+++ Assets/MothBoids.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MothBoids : MonoBehaviour
@@ -8,8 +9,14 @@ public class MothBoids : MonoBehaviour
     public GameObject prefabToInstantiate;
     public int numberOfInstances = 5;
 
+    public GameObject boundsObject;
+
+    private Bounds bounds;
+
     void Start()
     {
+        Renderer renderer = boundsObject.GetComponent<Renderer>();
+        bounds = renderer.bounds;
         // Call a method to spawn the objects
         SpawnObjects();
     }
@@ -23,7 +30,7 @@ public class MothBoids : MonoBehaviour
             for (int i = 0; i < numberOfInstances; i++)
             {
                 // Instantiate the prefab at a random position
-                Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
+                Vector3 randomPosition = SetRandomTargetPosition();
                 Instantiate(prefabToInstantiate, randomPosition, Quaternion.identity);
             }
         }
@@ -31,5 +38,14 @@ public class MothBoids : MonoBehaviour
         {
             Debug.LogError("Prefab not assigned in the inspector.");
         }
+    }
+
+    Vector3 SetRandomTargetPosition()
+    {
+        // Generate a random target position within a specified range
+        float randomX = Random.Range(bounds.center.x - bounds.extents.x, bounds.center.x + bounds.extents.x);
+        float randomY = Random.Range(bounds.center.y - bounds.extents.y, bounds.center.y + bounds.extents.y);
+        float randomZ = Random.Range(bounds.center.z - bounds.extents.z, bounds.center.z + bounds.extents.z);
+        return new Vector3(randomX, randomY, randomZ);
     }
 }
