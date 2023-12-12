d9a448afcb45c4dc1fa0b527138acd7b40272aa1
MireilleHossam <117849443+MireilleHossam@users.noreply.github.com>
2023-12-12 15:30:01 +0200

    Adjusting the code

--- Assets/Scripts/VREnemyController.cs
+++ Assets/Scripts/VREnemyController.cs
@@ -7,51 +7,76 @@ public class VREnemyController : MonoBehaviour
     public Transform player;
     public Animator enemy;
 
-    public float distanceThreshold = 3f;
-    public float attackingSpeed = 5f;
+    public float distanceThreshold = 10f;
+    public float attackingSpeed = 2f;
     public float walkingSpeed = 2f;
     public float deathSpeed = 2f;
+    public float idleSpeed = 2f;
+
+    private bool isDead = false;
+
+    private void Start()
+    {
+        enemy.SetBool("IsIdle", true);
+    }
 
     void Update()
     {
-        // Calculate the distance between the player and the VR character
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (!isDead)
+        {
+            // Calculate the distance between the player and the VR character
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        // Determine movement and animation based on the distance
-        if (distanceToPlayer > distanceThreshold)
-        {
-            // Far from the player, play walking animation and move slowly
-            enemy.SetBool("isWalking", true);
-            enemy.SetBool("isAttacking", false);
-            enemy.SetBool("isDieing", false);
-            MoveCharacter(walkingSpeed);
-        }
-        else if (distanceToPlayer <= distanceThreshold && distanceToPlayer > 1.0f)
-        {
-            // Close to the player, play attacking animation and move quickly
-            enemy.SetBool("isWalking", false);
-            enemy.SetBool("isAttacking", true);
-            enemy.SetBool("isDieing", false);
-            MoveCharacter(attackingSpeed);
-        }
-        else
-        {
-            // Close to the player, play dying animation and move quickly
-            enemy.SetBool("isWalking", false);
-            enemy.SetBool("isAttacking", false);
-            enemy.SetBool("isDieing", true);
-            MoveCharacter(deathSpeed);
+            // Determine movement and animation based on the distance
+            if (distanceToPlayer > distanceThreshold)
+            {
+                // Far from the player, play walking animation and move slowly
+                SetMovementParameters(false, true, false, walkingSpeed);
+            }
+            else if (distanceToPlayer <= distanceThreshold && distanceToPlayer > 1.0f)
+            {
+                // Close to the player, play attacking animation and move quickly
+                SetMovementParameters(false, false, true, attackingSpeed);
+            }
+            else
+            {
+                // Close to the player, play dying animation and move quickly
+                SetMovementParameters(false, false, false, deathSpeed);
+            }
         }
     }
 
-    void MoveCharacter(float speed)
+    void SetMovementParameters(bool isIdle, bool isWalk, bool isAttack, float speed)
     {
+        // Set animation parameters
+        enemy.SetBool("IsIdle", isIdle);
+        enemy.SetBool("IsWalk", isWalk);
+        enemy.SetBool("IsAttack", isAttack);
+
         // Move the VR character forward in the direction of the player
         Vector3 direction = player.position - transform.position;
         direction.y = 0; // Keep the character level
         direction.Normalize();
         transform.Translate(direction * speed * Time.deltaTime, Space.World);
     }
+
+    // Add this method to handle collisions
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collided object has a specific tag (customize as needed)
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+            // Trigger death animation or perform other actions
+            PerformDeath();
+        }
+    }
+
+    private void PerformDeath()
+    {
+        // Implement your death animation or other actions here
+        // For example, you might play an animation, disable enemy controls, etc.
+        isDead = true;
+        enemy.SetTrigger("IsDeath");
+        Debug.Log("Enemy collided with death zone. Triggering death animation.");
+    }
 }
-
-
